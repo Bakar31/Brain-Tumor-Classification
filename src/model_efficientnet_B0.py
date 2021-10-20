4c2a97bb10e621d21ceca5772b2c0ec1e208d4dc
Bakar31 <abubakar1808031@gmail.com>
2021-10-20 12:55:47 +0600

    adding comments

--- src/model_efficientnet_B0.py
+++ src/model_efficientnet_B0.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras.layers import GlobalAveragePooling2D, BatchNormalization, Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.models import Model
-from tensorflow.keras.applications import EfficientNetB0, EfficientNetB1, EfficientNetB2, EfficientNetB3
+from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.optimizers import Adam
 from data_generators import *
 
+# building model
 def model_EfficientNetB0(path, lr = 0.001, dr_rate = 0.15):
     model = EfficientNetB0(include_top=False, weights=path)
     model.trainable = False
